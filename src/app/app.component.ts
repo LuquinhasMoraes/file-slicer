687bd162804ebe5aded01281e06dd4ace18b2ea3
Lucas Moraes <lucas_moraes2011@outlook.com>
2025-01-26 11:12:38 -0300

    feat: melhorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  file: File | null = null;
+  file: File = new File([], "");
   progress: number | null = null;
   files: any[] = [];
   total: number = 0;
@@ -15,24 +15,141 @@ export class AppComponent {
 
   form = this.formBuilder.group({
     chunckSize: ['', Validators.required],
-    neverSplitLine: [true]
+    neverSplitLine: [true],
   });
 
   chunkSize = 0;
   isDisabled = true;
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {}
+  showModal: boolean = false;
+  errorMessage: string = '';
+  errorDetail: string = '';
+
+  showError(message: string, detail: string): void {
+    this.errorMessage = message;
+    this.errorDetail = detail;
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
+
+  resetValues() {
+    this.files = [];
+    this.total = 0;
+    // this.realTotal = 0;
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
 
   setChunkSize(input: any) {
     this.chunkSize = Number(input.value);
-    if(this.chunkSize > 0) {
+    if (this.chunkSize > 0) {
       this.isDisabled = false;
     } else {
-      this.isDisabled = true
+      this.isDisabled = true;
+    }
+  }
+
+  canSplit(): boolean {
+    return !!this.file && this.file.size > 0 && !!this.file.name && !!this.file.type && this.form.valid;
+  }
+
+  validate() {
+    if(this.chunkSize > this.file.size) {
+      this.showError('Tamanho escolhido é muito grande', 'O tamanho da fatia não pode ser maior que o tamanho do arquivo')
+      return false;
+    } else if(this.chunkSize === 0) {
+      this.showError('Tamanho escolhido é muito pequeno', 'O tamanho da fatia não pode ser zero')
+      return false;
     }
 
+    return true;
+
+  }
+
+  splitFile() {
+
+    if(!this.validate()) {
+      return;
+    }
+
+    if (!this.form.value.neverSplitLine) {
+      this.resetValues();
+      const chunkSize = this.chunkSize;
+      const totalChunks = Math.ceil(this.file.size / chunkSize);
+
+      for (let i = 0; i < totalChunks; i++) {
+        const start = i * chunkSize;
+        const end = Math.min(start + chunkSize, this.file.size);
+        const chunk = this.file.slice(start, end);
+
+        const file: any = new File(
+          [chunk],
+          i + '_' + this.file.name,
+          {
+            type: 'text/plain',
+          }
+        );
+
+        console.log(file);
+
+        file.link = URL.createObjectURL(file);
+        this.files.push(file);
+        this.total += file.size;
+      }
+    } else {
+      this.resetValues();
+      const reader = new FileReader();
+
+      const chunkSize = this.chunkSize; //* 1024;
+      const utf8Encode = new TextEncoder();
+      let contentBuffer = ''; // Acumulador para os dados
+      const files: any = []; // Para armazenar os pedaços processados
+
+      // Função para criar e adicionar um novo chunk de arquivo
+      const addFileChunk = (content: string, index: number): void => {
+        const chunk: any = new Blob([content], { type: 'text/plain' });
+        chunk.link = URL.createObjectURL(chunk);
+        chunk.name = index + '_' + this.file?.name;
+        files.push(chunk);
+        this.total += chunk.size;
+      };
+
+      // Evento de carregamento do arquivo
+      reader.onload = (evt: ProgressEvent<FileReader>) => {
+        const fileContent = (evt.target?.result as string).split('\n');
+        fileContent.forEach((line, index) => {
+          const isLastLine = index === fileContent.length - 1;
+
+          if(utf8Encode.encode(line + '\n').byteLength > chunkSize) {
+            this.showError('Tamanho escolhido é muito pequeno', 'O tamanho não pode ser menor que o tamanho em bytes de cada linha')
+            return;
+          } else if (
+            utf8Encode.encode(contentBuffer + line + '\n').byteLength <=
+            chunkSize
+          ) {
+            contentBuffer += line + '\n';
+          } else {
+            // Armazena o chunk atual e inicia um novo buffer
+            addFileChunk(contentBuffer, index);
+            contentBuffer = line + '\n';
+          }
+
+          // Se for a última linha, armazena o buffer restante
+          if (isLastLine) {
+            addFileChunk(contentBuffer, index);
+          }
+        });
+
+        // Atualiza a lista de arquivos processados
+        this.files = files;
+      };
+
+      // Lê o arquivo como texto UTF-8
+      reader.readAsText(this.file, 'UTF-8');
+    }
   }
 
   onFileSelected(event: Event): void {
@@ -41,78 +158,9 @@ export class AppComponent {
     const input = event.target as HTMLInputElement;
 
     if (input.files && input.files.length > 0) {
-
-      if(!this.form.value.neverSplitLine) {
-        this.file = input.files[0];
-        const chunkSize = this.chunkSize;
-        const totalChunks = Math.ceil(this.file.size / chunkSize);
-
-        for (let i = 0; i < totalChunks; i++) {
-          const start = i * chunkSize;
-          const end = Math.min(start + chunkSize, this.file.size);
-          const chunk = this.file.slice(start, end);
-
-          const file: any = new File([chunk], 'Parte ' + i + ': ' + this.file.name, {
-            type: "text/plain",
-          });
-          file.link = URL.createObjectURL(chunk);
-          file.name = file.name;
-          this.files.push(chunk);
-          this.total += file.size;
-        }
-
-      } else {
-        const reader = new FileReader();
-        this.file = input.files[0];
-        this.realTotal = this.file.size;
-        const chunkSize = this.chunkSize //* 1024;
-        const utf8Encode = new TextEncoder();
-        let contentBuffer = ''; // Acumulador para os dados
-        const files: any = []; // Para armazenar os pedaços processados
-
-        // Função para criar e adicionar um novo chunk de arquivo
-        const addFileChunk = (content: string): void => {
-          const chunk: any = new Blob([content], { type: 'text/plain' });
-          chunk.link = URL.createObjectURL(chunk);
-          chunk.name = this.file?.name;
-          files.push(chunk);
-          this.total += chunk.size;
-        };
-
-        // Evento de carregamento do arquivo
-        reader.onload = (evt: ProgressEvent<FileReader>) => {
-          const fileContent = (evt.target?.result as string).split('\n');
-          fileContent.forEach((line, index) => {
-            const isLastLine = index === fileContent.length - 1;
-
-            // Adiciona a linha ao buffer, verificando o tamanho acumulado
-            if (
-              utf8Encode.encode(contentBuffer + line + '\n').byteLength <=
-              chunkSize
-            ) {
-              contentBuffer += line + '\n';
-            } else {
-              // Armazena o chunk atual e inicia um novo buffer
-              addFileChunk(contentBuffer);
-              contentBuffer = line + '\n';
-            }
-
-            // Se for a última linha, armazena o buffer restante
-            if (isLastLine) {
-              addFileChunk(contentBuffer);
-            }
-          });
-
-          // Atualiza a lista de arquivos processados
-          this.files = files;
-        };
-
-        // Lê o arquivo como texto UTF-8
-        reader.readAsText(this.file, 'UTF-8');
-      }
-      }
-
-
+      this.file = input.files[0];
+      this.realTotal = this.file.size;
+    }
   }
 
   private isLastIteration(i: number, contentFile: any): boolean {
